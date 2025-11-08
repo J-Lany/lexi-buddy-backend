9c38af343793611a6e372bb691ec1c406966a584
Daniella J-Lany <ddzheylani@gmail.com>
2025-11-08 21:55:21 +0300

    Add tiny fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MailModule } from './mail/mail.module';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, MailModule],
+  imports: [AuthModule, MailModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
