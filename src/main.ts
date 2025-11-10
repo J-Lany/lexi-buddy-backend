21ec5b79e1d8cd054eb7579a8eaeccc62db6d341
Daniella Dzheylani <126955317+J-Lany@users.noreply.github.com>
2025-11-10 14:21:44 +0300

    chore(16) Add cors (#20)

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,17 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const origins = process.env.CORS_ORIGINS
+    ? process.env.CORS_ORIGINS.split(',')
+    : [];
+
+  app.enableCors({
+    origin: origins,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
