b819893e2c5b937cae6e6a6366156d587e4684fb
Daniella Dzheylani <126955317+J-Lany@users.noreply.github.com>
2025-12-20 13:25:46 +0300

    add getAllStudents (#50)

--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -5,11 +5,12 @@ import { PrismaModule } from 'prisma/prisma.module';
 import { GroupRepository } from 'repositories/group-repository';
 import { StudentsService } from './students.service';
 import { StudentsController } from './students.controller';
+import { UserRepository } from 'repositories/user.repository';
 
 @Module({
   imports: [AuthModule, PrismaModule],
   controllers: [StudentsController],
-  providers: [StudentsService, GroupRepository],
+  providers: [StudentsService, GroupRepository, UserRepository],
   exports: [StudentsService],
 })
 export class StudentsModule {}
